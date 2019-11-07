08f0eb0a623ad69f581ee4ea8b50cb924caa5609
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-07 15:39:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,16 @@
 # Add  code here!
 def prime?(number)
   range = (2..(number - 1)).to_a
-  check = true
-  if number < 2
-    check = false
-  else
-    range.each do |x|
-      if number % x == 0
-        check = false
-        break
-      end
-    end
-  end
-  puts check
-end
+   check = true
+   if number < 2
+     check = false
+   else
+     range.each do |x|
+       if number % x == 0
+         check = false
+         break
+       end
+     end
+   end
+    puts check
+ end
